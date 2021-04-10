db4beae61a567f7f422c7c4717879571a1239cdc
Cristian Correia <cristianwesley.ti@gmail.com>
2021-04-10 20:56:21 -0300

    Creating Header

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from 'assets/logoCristianFlix.png';
+
+import { Container } from './styles';
+
+export function Header({ color }) {
+  return (
+    <Container color={color}>
+      <div className="headerLogo">
+        <Link to="/">
+          <img src={logo} alt="logo CristianFlix" />
+        </Link>
+      </div>
+      <div className="headerUser">
+        <Link to="/">
+          <img
+            src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+            alt="user CristianFlix"
+          />
+        </Link>
+      </div>
+    </Container>
+  );
+}
---
+++ src/app/components/Header/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  position: fixed;
+  z-index: 999;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  height: 70px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 30px;
+  background: ${props => (props.color ? 'var(--background)' : 'transparent')};
+  transition: all ease 0.5s;
+
+  .headerLogo {
+    height: 45px;
+    img {
+      height: 100%;
+    }
+  }
+
+  .headerUser {
+    height: 35px;
+    img {
+      height: 100%;
+      border-radius: 3px;
+    }
+  }
+`;
